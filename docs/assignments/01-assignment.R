5d6900383f181ea9118f8f607c76529f2067a65a
alopilato88 <alex.lopilato@gmail.com>
2023-09-11 22:39:31 -0400

    Update site with assignment 1 page

---
+++ docs/assignments/01-assignment.R
@@ -0,0 +1,75 @@
+### --- Assignment 1 --- ###
+
+Instructions: In this assignment you will 
+
+# Install and load R packages
+install.packages("tidyverse")
+
+# Load required R packages
+library(tibble)
+
+# Read in the data from the class site
+data_ai <- readr::read_csv()
+
+# View our data frame 
+View(data_ai)
+
+# 1. What is the sample size of your dataset (hint: It's the number of rows)? 
+
+nrow(data_ai)
+
+# 2. How many variables are in your dataset (hint: It's the number of columns)? 
+
+ncol(data_ai)
+
+# 3. What is the mean and standard deviation of perceived_ease_use?
+
+mean(data_ai$perceived_ease_use) # mean() is a function that calculates the mean of a rando variable.
+sd(data_ai$perceived_ease_use) # sd() is a function that calculates the standard deviation of a random variable.
+
+# 4. What is the mean and standard deviation for perceived_useful?
+
+# 5. What is the mean and standard deviation for behavioral_intention?
+
+# 6a. What is the correlation between perceived_useful and perceived_ease_of_use? 
+
+# cor() is a function that calculates the correlation between two random variables
+cor(x = data_ai$perceived_useful, y = data_ai$perceived_ease_use) 
+
+# 6b. In your own words, write out an interpretation of the correlation you calculated in 6a. 
+
+# 7a. What is the correlation between perceived_useful and behavioral_intention? 
+
+# 7b. In your own words, write out an interpretation of the correlation you calculated in 7a. 
+
+# 8a. What is the correlation between perceived_ease_use and behavioral_intention? 
+
+# 8b. In your own words, write out an interpretation of the correlation you calculated in 8a. 
+
+# 9a. Estimate a simple regression model that uses perceived_ease_use to predict behavioral_intention.
+
+model_1 <- lm(behavioral_intention ~ perceived_ease_use, data = data_ai)
+
+# 9b. Print out the results of your regression model and write out an interpretation of the effect of perceived_ease_use on
+#     behavioral_intentions.
+
+summary(model_1)
+
+# 9c. What is the R-squared value for model_1? Write out an interpretation of the R-squared.
+
+# 10a. Estimate a simple regression model that uses perceived_useful to predict behavioral_intention. Name the model: model_2.
+
+# 10b. Print out the results of your regression model and write out an interpretation of the effect of perceived_useful on
+#     behavioral_intentions.
+
+# 10c. What is the R-squared value for model_2? Write out an interpretation of the R-squared.
+
+# 11a. Estimate a multiple regression model that uses perceived_useful and perceived_ease_use to predict behavioral_intention.
+
+model_3 <- lm(behavioral_intention ~ perceived_ease_use + perceived_useful, data = data_ai)
+
+# 11b. Print out the results of your multiple regression model and write out an interpretation of both partial regression 
+#      coefficients.
+
+# 11c. What is the R-squared value for model_3? Write out an interpretation of the R-squared.
+
